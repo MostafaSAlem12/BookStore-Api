29e91053c9d8543322997d3725d92fa334ece355
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2023-07-11 15:55:16 +0300

    jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const { User, validateRegisterUser, validateLoginUser } = require("../models/User");
-const bcrypt = require("bcryptjs")
 
 
 /**
@@ -33,10 +34,37 @@ router.post('/register', asyncHandler(async (req, res) => {
   });
 
   const result = await user.save();
-  const token = null;
+  const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET_KEY);;
   const { password, ...other } = result._doc;
   res.status(201).json({ ...other, token });
 }))
 
+
+/**
+ * @desc Login User
+ * @route /api/auth/login
+ * @method POST
+ * @access public
+ */
+
+router.post('/login', asyncHandler(async (req, res) => {
+  const { error } = validateLoginUser(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    res.status(400).json({ message: "Invalid Email or Password" })
+  }
+
+  const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+
+  if (!isPasswordMatch) {
+    return res.status(400).json({ message: "Invalid Email or Password" })
+  }
+  const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET_KEY);
+  const { password, ...other } = user._doc;
+  res.status(200).json({ ...other, token });
+}))
 
 module.exports = router;
