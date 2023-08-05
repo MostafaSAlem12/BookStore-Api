e9329d940392331025783408e864100a29f5a193
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2023-08-05 08:44:06 +0300

    get reset password view

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const {User} = require("../models/User")
+const { User } = require("../models/User")
 const jwt = require('jsonwebtoken')
 
 /**
@@ -20,12 +20,47 @@ module.exports.getForgotPasswordView = asyncHandler((req, res) => {
  * @access public
  */
 
-module.exports.sendForgotPasswordLink = asyncHandler( async (req, res) => {
+module.exports.sendForgotPasswordLink = asyncHandler(async (req, res) => {
     console.log(req.body.email);
-   const user = await User.findOne({email: req.body.email})
-   if(!user){
-    return res.status(404).json({message : 'User Not Found'})
-   }
-   const secret = process.env.JWT_SECRET_KEY + user.password; 
-   const token = jwt.sign({email:user.email, id: user.id},secret)
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(404).json({ message: 'User Not Found' })
+    }
+    const secret = process.env.JWT_SECRET_KEY + user.password;
+    const token = jwt.sign({ email: user.email, id: user.id }, secret, { expiresIn: '10m' })
+
+
+    const link = `http://localhost:5000/password/reset-password/${User._id}/${token}`;
+
+    res.json({ message: 'Click on the link', resetPasswordLink: link })
+
+    //TODO : send email to the user 
+})
+
+/**
+ * @desc Get Reset password link
+ * @route /password/reset-password/:userId/token
+ * @method GET
+ * @access public
+ */
+
+module.exports.getResetPasswordView = asyncHandler(async (req, res) => {
+    console.log(req.body.email);
+    const user = await User.findById(req.params.userId)
+    if (!user) {
+        return res.status(404).json({ message: 'User Not Found' })
+    }
+    const secret = process.env.JWT_SECRET_KEY + user.password;
+    try {
+        jwt.verify(req.params.token, secret);
+        res.render('reset-password', { email: user.email })
+    } catch (error) {
+        console.log(error);
+        res.json({ message: "error" });
+
+    }
+
+    const link = `http://localhost:5000/password/reset-password/${User._id}/${token}`;
+
+    res.json({ message: 'Click on the link', resetPasswordLink: link })
 })
