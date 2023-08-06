d85ce57d9a457f3572382fd752184241db4310d2
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2023-08-06 17:13:19 +0300

    upload Image

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const logger = require('./middlewares/logger')
-const connectToDB =require('./config/db')
+const connectToDB = require('./config/db')
 require("dotenv").config();
 const { notFound, errHandler } = require("./middlewares/errors");
 const { setRandomFallback } = require("bcryptjs");
+const path = require("path")
 
 //connection to Database
 connectToDB();
@@ -13,19 +14,23 @@ const app = express();
 
 // Apply Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(logger);
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
+app.use("/images", express.static(path.join(__dirname, "images")));
 //Routes 
 app.get('/', (req, res) => {
   res.send("WELCOME IN BOOKstore API  ")
 })
+
+app.use("/api/upload", require("./routes/upload"))
 app.use("/api/books", require("./routes/books"))
 app.use("/api/authors", require("./routes/authors"))
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/users", require("./routes/users"))
-app.use("/password",require('./routes/password'));
+// app.use("/api/upload", require("./routes/upload"))
+app.use("/password", require('./routes/password'));
 
 // Error Handling
 app.use(notFound);
