e637d2f9d5b6ce5b24335974a60186ac904425be
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2023-08-06 17:18:09 +0300

    fix image upload err

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ connectToDB();
 //init App
 const app = express();
 
+//static folder
+app.use(express.static(path.join(__dirname, "images")));
+
 // Apply Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(logger);
 app.set('view engine', 'ejs')
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+
 //Routes 
 app.get('/', (req, res) => {
   res.send("WELCOME IN BOOKstore API  ")
@@ -29,7 +32,6 @@ app.use("/api/books", require("./routes/books"))
 app.use("/api/authors", require("./routes/authors"))
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/users", require("./routes/users"))
-// app.use("/api/upload", require("./routes/upload"))
 app.use("/password", require('./routes/password'));
 
 // Error Handling
